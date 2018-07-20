0462fc48fe0322c900ae9e0599b9cb921eb32366
EvilBobarino <40873080+EvilBobarino@users.noreply.github.com>
2018-07-20 11:37:27 +0100

    ddddddddddddddfug

--- lua/lib/tweak_data/charactertweakdata.lua
+++ lua/lib/tweak_data/charactertweakdata.lua
@@ -4,40 +4,9 @@ local origin_presets = CharacterTweakData._presets
 function CharacterTweakData:init(tweak_data)
 	local presets = self:_presets(tweak_data)
 	origin_init(self, tweak_data)
-	if job == "mad" then
+	if job == "mad" or "hvh" then
 		self.taser.custom_voicework = nil
 	else
 		self.taser.custom_voicework = "taser"
 	end	
-	if job == "mad" then
-		self.cop.custom_voicework = nil
-	else
-		self.cop.custom_voicework = "light"
-	end	
-	if job == "mad" then
-		self.fbi_swat.custom_voicework = nil
-	else
-		self.fbi_swat.custom_voicework = "light"
-	end	
-	if job == "mad" then
-		self.swat.custom_voicework = nil
-	else
-		self.swat.custom_voicework = "light"
-	end	
-	if job == "mad" then
-		self.city_swat.custom_voicework = nil
-	else
-		self.city_swat.custom_voicework = "heavy"
-	end	
-	if job == "mad" then
-		self.heavy_swat.custom_voicework = nil
-	else
-		self.heavy_swat.custom_voicework = "heavy"
-	end	
-	if job == "mad" then
-		self.fb_heavy_swat.custom_voicework = nil
-	else
-		self.fb_heavy_swat.custom_voicework = "heavy"
-	end
-	self.
 end
